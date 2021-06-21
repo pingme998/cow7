b404f51732556dcfcd813c898882fe3d025cea77
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-06-21 11:26:51 +0000

    Add files via upload

---
+++ Essential-Files/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+wget 'https://gist.githubusercontent.com/developeranaz/fb2150cc762fcdb7045dfe83a1439de3/raw/9a80dfaed40e9120a75513b1a1eb9d90f0914bf3/rclon.conf' -O /.config/rclone/rclone.conf
+rclone version
+rclone listremotes > remotesch.txt
+sed 's/.$//' remotesch.txt > remotes.txt
+while read line
+do
+   my_var="$my_var, '$line'"
+done < remotes.txt
+
+my_var="${my_var#, }"              
+
+echo "$my_var"
+
+OLD=REMOTCLOUD
+sed "s|$OLD|$my_var|g" /Aria2Rclone.htpy >/Aria2Rclone.ipynb
+
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
+jupyter serverextension enable voila --sys-prefix
+curl 'https://raw.githubusercontent.com/developeranaz/Aria2-Rclone-Remote-Uploader-HEROKU/main/Essential-Files/d/donate.developeranaz.txt'
+voila --port=$PORT --no-browser  /Aria2Rclone.ipynb --theme=dark
